6ffd029d13f3bab9ce588f08f2d3d3ea68d3f6d1
danielpgp1012 <daniel.pachecogutierrez@epfl.ch>
2020-02-19 14:06:08 +0100

    Add pizza problem class
    Generate templates

--- practice_round/solution.py
+++ practice_round/solution.py
@@ -1,11 +1,29 @@
 import pandas as pd
 import numpy as np
 
-def read_input(file_name):
-    """TODO: parse input"""
-    return 1
 
+class PizzaProblem():
+    pizzas = np.NaN ## contains array with pizzas
+    M = 0 # number of slices
+    N = 0 #number of pizzas
+    chosenPizzas = np.NaN
+    score = 0
+    def __init__(self,file_name):
+        self.read_input(file_name)
+
+    def read_input(self,file_name):
+        """TODO: parse input"""
+        with open(file_name) as f:
+            M,N = f.readline().split()
+            pizzas = np.array(f.readline().split())
+
+    def findPizzasRecursive(self):
+        """TODO: sort pizzas in descending order. Implement dynamic programming top_down"""
+    def findPizzasBottomUp(self):
+        """TODO: sort pizzas in descending order. Implement bottom_up DP """
+    def printOutput(self):
+        """TODO: Print output in form of submission file """
 if __name__ == "__main__":
-    a = read_input('a')
+    pizza_problem
     print(a)
 
--- practice_round/tests.py
+++ practice_round/tests.py
@@ -5,6 +5,7 @@ class MyTestCase(unittest.TestCase):
     def test_something(self):
         self.assertEqual(True, False)
     def test_problema(self):
+        """to do"""
         a = s.read_input()
 
 
