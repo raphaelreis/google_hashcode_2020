49b2aa6923d301ad6f0e25d27b4fe488729efe56
danielpgp1012 <daniel.pachecogutierrez@epfl.ch>
2020-02-20 21:12:27 +0100

    Update solution.py

--- qualification_round/solution.py
+++ qualification_round/solution.py
@@ -46,7 +46,7 @@ class LibrarySystem:
         self.libs_df = pd.DataFrame(zip(t,b,b_ids),columns=['t','b','b_ids'])
         self.libs_df['taken_books'] = 0
         self.libs_df['score']=0
-
+        self.libs_df['b_ids'].apply(lambda l: np.array(l))
 
 
     def print_output(self):
@@ -79,24 +79,6 @@ class LibrarySystem:
 if __name__ == "__main__":
     file_name = "a_example.txt"
 
-    #s = parse_input(file_name)
     system = LibrarySystem(file_name)
-    #B, L, D = get_constants(s[0])
-    #W = get_books_weight(s[1])
-
-    # t = []
-    # books_count = []
-    # b = []
-    # b_ids = []
-
-    # for i in range(2, 3 + L, 2):
-    #     print(i)
-    #     lib_setup = [int(i) for i in s[i].split()]
-    #     books_count.append(lib_setup[0])
-    #     t.append(lib_setup[1])
-    #     b.append(lib_setup[2])
-    #     b_ids.append([int(i) for i in s[i+1].split()])
-
-    #books_count, t, b, b_ids = parse_libraries(s, L)
-
-    #print(books_count, t, b, b_ids)
+    print(system.libs_df)
+    system.libs_df.apply(system.compute_score,axis=1)
