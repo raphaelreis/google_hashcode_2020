4a7e51b29be4635525457513c517096dbaef5aae
danielpgp1012 <daniel.pachecogutierrez@epfl.ch>
2020-02-20 20:09:54 +0100

    Add input files, class

--- qualification_round/solution.py
+++ qualification_round/solution.py
@@ -4,6 +4,17 @@ def parse_input(filename):
     """TODO: Parse input file"""
 def print_output():
     """TODO: Print output as requested"""
+class LibrarySystem:
+
+    def __init__(self,file_id):
+        self.lib_df = self.parse_input(file_id)
+
+
+    def parse_input(filename):
+        """TODO: Parse input file"""
+
+    def print_output():
+        """TODO: Print output as requested"""
 
 
 if __name__ == "__main__":
