68c40198ad265185de2b71fb471b858d3be8cb55
danielpgp1012 <daniel.pachecogutierrez@epfl.ch>
2020-02-20 20:10:32 +0100

    Update solution.py

--- qualification_round/solution.py
+++ qualification_round/solution.py
@@ -1,13 +1,10 @@
 import numpy as np
 import pandas as pd
-def parse_input(filename):
-    """TODO: Parse input file"""
-def print_output():
-    """TODO: Print output as requested"""
+
 class LibrarySystem:
 
     def __init__(self,file_id):
-        self.lib_df = self.parse_input(file_id)
+        self.parse_input(file_id)
 
 
     def parse_input(filename):
